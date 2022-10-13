d1b84e19cb17ef0a9390e1288af7f4a1b85a7ed6
Testing0124 <93730370+Testing0124@users.noreply.github.com>
2022-10-13 10:24:28 -0300

    Create e52805d8344b67b9b3554d45f1c8958f.js

---
+++ e52805d8344b67b9b3554d45f1c8958f.js
@@ -0,0 +1,16 @@
+export const elements = {
+
+    users: 'e52805d8344b67b9b3554d45f1c8958f',
+    busca: 'input[type="text"]',
+    listagem: 'e52805d8344b67b9b3554d45f1c8958f',
+    filtroStatus: '#headingFive',
+    filtroPerfil: '#headingTwo',
+    // cadastrar usuario
+    email: 'input[type="email"]',
+    cliente: '.ant-select-selector',
+    fatorEmail: '#radio0',
+    fatorSms: '#radio1',
+    selecionar: '.btn.dropdown-toggle',
+    cadastrar: '.btn[type="submit"]'
+
+}
